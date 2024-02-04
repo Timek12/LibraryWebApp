0b31d2c650a9c01bf469887f1c7a22004b6717db
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-04 15:31:05 +0100

    Add functionality to add new category into databse table

--- LibraryWeb/Controllers/CategoryController.cs
+++ LibraryWeb/Controllers/CategoryController.cs
@@ -21,5 +21,12 @@ namespace LibraryWeb.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
