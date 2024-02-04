18bb54464c8a283ded249681571236868ea99e75
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-04 23:46:24 +0100

    Add functionality to edit category

--- LibraryWeb/Controllers/CategoryController.cs
+++ LibraryWeb/Controllers/CategoryController.cs
@@ -38,5 +38,35 @@ namespace LibraryWeb.Controllers
 
             return View(obj);
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            //var category = _db.Categories.Find(id);
+            //var category = _db.Categories.Where(c => c.Id == id).FirstOrDefault();
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
     }
 }
