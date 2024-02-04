64929677b7b209d4df5f9fec25644fe1116ba927
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-04 01:11:34 +0100

    Create and add category table to database

--- LibraryWeb/Models/Category.cs
+++ LibraryWeb/Models/Category.cs
@@ -4,7 +4,9 @@ namespace LibraryWeb.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
     }
