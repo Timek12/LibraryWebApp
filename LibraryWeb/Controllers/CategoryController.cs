0061af1d664207b0eecc5f6b6bca38f96c42494f
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-04 13:31:07 +0100

    Seed category table with sample data

--- LibraryWeb/Controllers/CategoryController.cs
+++ LibraryWeb/Controllers/CategoryController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LibraryWeb.Data;
+using LibraryWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LibraryWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            List<Category> categoryList = _db.Categories.ToList();
             return View();
         }
     }
--- LibraryWeb/Data/ApplicationDbContext.cs
+++ LibraryWeb/Data/ApplicationDbContext.cs
@@ -5,11 +5,21 @@ namespace LibraryWeb.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
-        { 
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
 
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "History", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Fantasy", DisplayOrder = 3 }
+                );
+        }
     }
 }
+
