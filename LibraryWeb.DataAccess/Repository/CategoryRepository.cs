4eae212d8a95d4c68a32c82e6c03ef5c5d892b36
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-06 22:54:56 +0100

    Implement CategoryRepository using base generic repository related interface

---
+++ LibraryWeb.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using LibraryWeb.DataAccess.Data;
+using LibraryWeb.DataAccess.Repository.IRepository;
+using LibraryWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryWeb.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category category)
+        {
+            _db.Update(category);
+        }
+    }
+}
