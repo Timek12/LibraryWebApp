6e86dab87f70576c4ddf23014386203aa8e04754
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-06 22:49:26 +0100

    Implement ICategoryRepository and append update and save methods declarations

---
+++ LibraryWeb.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using LibraryWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryWeb.DataAccess.Repository.IRepository
+{
+    internal interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category category);
+        void Save();
+    }
+}
