b562b883a795f6c6e071be1e87ff877197d552c5
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-07 20:43:31 +0100

    Add image url column to product table

--- LibraryWeb.Models/Product.cs
+++ LibraryWeb.Models/Product.cs
@@ -53,5 +53,7 @@ namespace LibraryWeb.Models
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
         public Category Category { get; set; }
+
+        public string ImageUrl { get; set; }
     }
 }
