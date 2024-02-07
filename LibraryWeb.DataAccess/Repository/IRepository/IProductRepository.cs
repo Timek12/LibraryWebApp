144d364557825635a6e7211cbf692f897b192c59
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-07 16:32:40 +0100

    Create product repository class and interface, update unit of work repository

--- LibraryWeb.DataAccess/Repository/IRepository/IProductRepository.cs
+++ LibraryWeb.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,8 @@ using System.Threading.Tasks;
 
 namespace LibraryWeb.DataAccess.Repository.IRepository
 {
-    public interface IUnitOfWork
+    public interface IProductRepository
     {
-        ICategoryRepository Category { get; }
-        IProductRepository Product { get; }
-        void Save();
+        void Update(Product product);
     }
 }
---
+++ LibraryWeb.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using LibraryWeb.DataAccess.Data;
+using LibraryWeb.DataAccess.Repository.IRepository;
+using LibraryWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryWeb.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product product)
+        {
+            _db.Update(product);
+        }
+    }
+}
