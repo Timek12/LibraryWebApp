df89e1fb782b6dce8a053340be908ea82c007efe
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-07 14:53:32 +0100

    Add product model

---
+++ LibraryWeb.Models/Product.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryWeb.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [DisplayName("Category Name")]
+        public string Title { get; set; }
+
+        [MaxLength(500)]
+        public string Description { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string ISBN { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Author { get; set; }
+
+        [Required]
+        [Display(Name = "List Price")]
+        [Range(1, 1000)]
+        public double ListPrice { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 1-50")]
+        [Range(1, 1000)]
+        public double Price { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 50+")]
+        [Range(1, 1000)]
+        public double Price50 { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 100+")]
+        [Range(1, 1000)]
+        public double Price100 { get; set; }
+    }
+}
