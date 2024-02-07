554c05bef29890126334424ac14e90d0e11f2fcc
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-07 17:29:13 +0100

    Create temporary index view page of the products

---
+++ LibraryWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using LibraryWeb.DataAccess.Repository.IRepository;
+using LibraryWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+
+namespace LibraryWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public ActionResult Index()
+        {
+            List<Product> productsList = _unitOfWork.Product.GetAll().ToList();
+            return View(productsList);
+        }
+
+
+
+    }
+}
