7abe456dba9822b48def8fff0f69f22412099f2f
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-08 13:51:59 +0100

    Create functionality for uploading and storing image entered in create product view

--- LibraryWeb/Areas/Admin/Controllers/ProductController.cs
+++ LibraryWeb/Areas/Admin/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Identity.Client;
 using LibraryWeb.Models.ViewModels;
+using Microsoft.Extensions.Logging.Abstractions;
 
 namespace LibraryWeb.Areas.Admin.Controllers
 {
@@ -11,9 +12,11 @@ namespace LibraryWeb.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public ActionResult Index()
@@ -50,6 +53,20 @@ namespace LibraryWeb.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if(file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images\product");
+
+                    using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+
+                    productVM.Product.ImageUrl = @"\images\product\" + fileName;
+                }
+
                 _unitOfWork.Product.Add(productVM.Product);
                 _unitOfWork.Save();
                 TempData["success"] = "Product created successfully!";
