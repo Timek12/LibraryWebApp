f1886ffbf1e7baafd0fccd196133c06d6b02da5d
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-08 14:12:52 +0100

    Add functionality to not update image when user does not edit image input field

--- LibraryWeb/Areas/Admin/Controllers/ProductController.cs
+++ LibraryWeb/Areas/Admin/Controllers/ProductController.cs
@@ -59,6 +59,15 @@ namespace LibraryWeb.Areas.Admin.Controllers
                     string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
                     string productPath = Path.Combine(wwwRootPath, @"images\product");
 
+                    if(!string.IsNullOrEmpty(productVM.Product.ImageUrl)) {
+                        var oldImagePath = Path.Combine(wwwRootPath, productVM.Product.ImageUrl.TrimStart('\\'));
+
+                        if(System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
                     using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
                     {
                         file.CopyTo(fileStream);
@@ -67,7 +76,17 @@ namespace LibraryWeb.Areas.Admin.Controllers
                     productVM.Product.ImageUrl = @"\images\product\" + fileName;
                 }
 
-                _unitOfWork.Product.Add(productVM.Product);
+                if(productVM.Product.Id == 0)
+                {
+                    // add functionality
+                    _unitOfWork.Product.Add(productVM.Product);
+                }
+                else
+                {
+                    // edit functionality
+                    _unitOfWork.Product.Update(productVM.Product);
+                }
+
                 _unitOfWork.Save();
                 TempData["success"] = "Product created successfully!";
                 return RedirectToAction("Index");
