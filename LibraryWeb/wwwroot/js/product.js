ba2a3f784d93da360b22bc0b6bbb36211c08e63e
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-09 13:03:27 +0100

    Change bootstrap theme

--- LibraryWeb/wwwroot/js/product.js
+++ LibraryWeb/wwwroot/js/product.js
@@ -7,24 +7,24 @@ function loadDataTable() {
     dataTable = $('#tblData').DataTable({
         "ajax": { url: '/admin/product/getall' },
         "columns": [
-            { data: 'title', "width": "20%" },
+            { data: 'title', "width": "25%" },
             { data: 'isbn', "width": "15%" },
             { data: 'listPrice', "width": "10%" },
             { data: 'author', "width": "15%" },
-            { data: 'category.name', "width": "15%" },
+            { data: 'category.name', "width": "10%" },
             {
-                data: 'id', 
+                data: 'id',
                 "render": function (data) {
                     return `<div class="btn-group" role="group">
-                            <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i>Edit</a>
-                            <a onClick=Delete('/admin/product/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i>Delete</a>
-                            </div >`
+                     <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>               
+                     <a onClick=Delete('/admin/product/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+                    </div>`
                 },
-                "width": "30%"
-            },
+                "width": "25%"
+            }
         ]
     });
-};
+}
 
 function Delete(url) {
     Swal.fire({
@@ -40,7 +40,7 @@ function Delete(url) {
             $.ajax({
                 url: url,
                 type: 'DELETE',
-                success: function (deta) {
+                success: function (dta) {
                     dataTable.ajax.reload();
                     Swal.fire({
                         title: "Deleted!",
