1d6a864967796a1238c9d9668a9521d0c57d49b1
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-12 18:34:12 +0100

    Add fields for additional properties in register UI

--- LibraryWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ LibraryWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -108,6 +108,14 @@ namespace LibraryWeb.Areas.Identity.Pages.Account
             public string? Role { get; set; }
             [ValidateNever]
             public IEnumerable<SelectListItem> RoleList { get; set; }
+            public string? PhoneNumber { get; set; }
+            [Required]
+            public string Name { get; set; }
+
+            public string? StreetAddress { get; set; }
+            public string? City { get; set; }
+            public string? PostalCode { get; set; }
+
 
         }
 
@@ -145,6 +153,11 @@ namespace LibraryWeb.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                user.StreetAddress = Input.StreetAddress;
+                user.City = Input.City;
+                user.PostalCode = Input.PostalCode;
+                user.PhoneNumber = Input.PhoneNumber;
+                user.Name = Input.Name;
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
