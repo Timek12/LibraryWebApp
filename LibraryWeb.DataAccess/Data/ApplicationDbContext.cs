38ced1cb64ad279d5f7cd3d94c13697518738507
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-12 15:40:17 +0100

    Extend default identity user to add more columns

--- LibraryWeb.DataAccess/Data/ApplicationDbContext.cs
+++ LibraryWeb.DataAccess/Data/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ namespace LibraryWeb.DataAccess.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users {  get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
