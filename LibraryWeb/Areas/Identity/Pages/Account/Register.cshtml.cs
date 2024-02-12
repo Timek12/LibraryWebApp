4c9dc7ae5bfdfc62e0f7cec707182b5c9d39e480
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-12 17:12:00 +0100

    Add negation when checking if customer role exists

--- LibraryWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ LibraryWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -107,7 +107,7 @@ namespace LibraryWeb.Areas.Identity.Pages.Account
 
         public async Task OnGetAsync(string returnUrl = null)
         {
-            if(_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
+            if(!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
             {
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Company)).GetAwaiter().GetResult();
