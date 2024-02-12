9b7369248d26092bfc25894f98b3795ef444202d
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-12 16:52:50 +0100

    Add application roles as const string

--- LibraryWeb.Utility/SD.cs
+++ LibraryWeb.Utility/SD.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,9 @@ namespace LibraryWeb.Utility
 {
     internal class SD
     {
+        public const string Role_User_Cust = "Customer";
+        public const string Role_User_Comp = "Company";
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
     }
 }
