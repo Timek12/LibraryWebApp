fb25c2ef8693c25ab51eeaf04a53c83cb8ee6867
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-12 15:54:10 +0100

    Change funcitonality to register as user model instead of indenityUser model

--- LibraryWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ LibraryWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading;
 using System.Threading.Tasks;
+using LibraryWeb.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -154,11 +155,11 @@ namespace LibraryWeb.Areas.Identity.Pages.Account
             return Page();
         }
 
-        private IdentityUser CreateUser()
+        private User CreateUser()
         {
             try
             {
-                return Activator.CreateInstance<IdentityUser>();
+                return Activator.CreateInstance<User>();
             }
             catch
             {
